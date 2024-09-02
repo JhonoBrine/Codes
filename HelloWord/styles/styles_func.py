2b35d0f5aff94e648c79fea2274a2294bbf8a5e3
JhonoBrine <jhonpabroa@gmail.com>
2024-09-03 01:10:54 +0800

    asdafa

--- HelloWord/styles/styles_func.py
+++ HelloWord/styles/styles_func.py
@@ -1,21 +1,21 @@
 import streamlit as st
 
-def load_css(file_path):
-    with open(file_path) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# def load_css(file_path):
+#     with open(file_path) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-load_css('styles/styles.css')
+# load_css('styles/styles.css')
 
-def styled_experience(school, link, course, role, image, date, project):
-    return f"""
-    <div class="experience-container">
-        <img src="{image}" class="experience-image" alt="{school}" />
-        <div class="experience-content">
-            <h3><a href="{link}" class="experience-school">{school}</a></h3>
-            <p class="experience-course">{course}</p>
-            <p class="experience-role">{role}</p>
-            <p class="experience-date">{date}</p>
-            <p class="experience-project">{project}</p>
-        </div>
-    </div>
-    """
+# def styled_experience(school, link, course, role, image, date, project):
+#     return f"""
+#     <div class="experience-container">
+#         <img src="{image}" class="experience-image" alt="{school}" />
+#         <div class="experience-content">
+#             <h3><a href="{link}" class="experience-school">{school}</a></h3>
+#             <p class="experience-course">{course}</p>
+#             <p class="experience-role">{role}</p>
+#             <p class="experience-date">{date}</p>
+#             <p class="experience-project">{project}</p>
+#         </div>
+#     </div>
+#     """
